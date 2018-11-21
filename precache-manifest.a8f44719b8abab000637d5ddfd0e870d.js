f2b1e72e9bf842098697c964f644276163e2d61a
Roger Rengifo <rogerreng.c@gmail.com>
2018-11-21 18:49:01 -0400

    Updates

---
+++ precache-manifest.a8f44719b8abab000637d5ddfd0e870d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "4918e1492526a96242981765fe4321ae",
+    "url": "/huddle/static/media/icon-communities.4918e149.svg"
+  },
+  {
+    "revision": "0e26bd8e2112d1d27ba4",
+    "url": "/huddle/static/css/main.ba490531.chunk.css"
+  },
+  {
+    "revision": "c90d96c2492a2d6f0863256f2bf9015d",
+    "url": "/huddle/static/media/icon-email.c90d96c2.svg"
+  },
+  {
+    "revision": "826bcf874a9e67bfc56c",
+    "url": "/huddle/static/js/1.826bcf87.chunk.js"
+  },
+  {
+    "revision": "429adb545bb690e08811",
+    "url": "/huddle/static/js/runtime~main.429adb54.js"
+  },
+  {
+    "revision": "144da12e04f88d8d639f022bc8f997aa",
+    "url": "/huddle/static/media/bg-footer-top-mobile.144da12e.svg"
+  },
+  {
+    "revision": "88090acec5276438b135a468364edf12",
+    "url": "/huddle/static/media/screen-mockups.88090ace.svg"
+  },
+  {
+    "revision": "0e26bd8e2112d1d27ba4",
+    "url": "/huddle/static/js/main.0e26bd8e.chunk.js"
+  },
+  {
+    "revision": "1203b299cf1910559663db88126d869e",
+    "url": "/huddle/static/media/icon-messages.1203b299.svg"
+  },
+  {
+    "revision": "bb27da81d1984faa5ef57668534ff1a5",
+    "url": "/huddle/static/media/illustration-grow-together.bb27da81.svg"
+  },
+  {
+    "revision": "6641e825f006d463db7028607e3ee994",
+    "url": "/huddle/static/media/illustration-flowing-conversation.6641e825.svg"
+  },
+  {
+    "revision": "8a5af9952fb5ed23779d589621b108a5",
+    "url": "/huddle/static/media/illustration-your-users.8a5af995.svg"
+  },
+  {
+    "revision": "4c8c2ba2f1cc804f9092d504c9b0decd",
+    "url": "/huddle/static/media/icon-phone.4c8c2ba2.svg"
+  },
+  {
+    "revision": "826bcf874a9e67bfc56c",
+    "url": "/huddle/static/css/1.dfd34445.chunk.css"
+  },
+  {
+    "revision": "05bb791fdac826e6a539af2a8add8bdb",
+    "url": "/huddle/index.html"
+  }
+];
